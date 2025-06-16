0c20c2e830760704393a59e7c4f94b203da77c6c
Jao2w <joao.lima.portes@escola.pr.gov.br>
2025-06-16 07:38:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const startButton = document.querySelector(".start")
+const gameOverscreen = document.querySelector(".game-over")
+
+let gameStarted = false;
+
+const startGame = () => {
+    gameStarted = true
+
+    pipe.style.animation = "pipe-animation 1.5s infinite linear";
+
+    startButton.style.display = "none";
+}
 
 const jump = () => {
     mario.classList.add(".jump")
@@ -24,6 +36,17 @@ const loop = setInterval(() => {
         mario.src = "./img/game-over.png";
         mario.style.width = "75px";
         mario.style.marginleft = "50px";
+
+
+
+
+
+
+
+
+        const restart = () => {
+            window.location.reload();
+        }
 
         clearInterval(loop);
     }
